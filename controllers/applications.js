f9a4c6c390f85f7af68853b735399ea9b55e8231
Sleeper9235 <kylejyoumans@gmail.com>
2024-06-27 09:24:37 -0500

    modify application index page

--- controllers/applications.js
+++ controllers/applications.js
@@ -3,9 +3,12 @@ const router = express.Router()
 
 const User = require('../models/user')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.render('applications/index.ejs')
+        const currentUser = await User.findById(req.session.user._id)
+        res.render('applications/index.ejs', {
+            applications: currentUser.applications,
+        })
     } catch (error) {
         console.log(error)
         res.redirect('/')
@@ -18,20 +21,18 @@ router.get('/new', (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-      // Look up the user from req.session
-      const currentUser = await User.findById(req.session.user._id);
-      // Push req.body (the new form data object) to the
-      // applications array of the current user
-      currentUser.applications.push(req.body);
-      // Save changes to the user
-      await currentUser.save();
-      // Redirect back to the applications index view
-      res.redirect(`/users/${currentUser._id}/applications`);
-    } catch (error) {
-      // If any errors, log them and redirect back home
-      console.log(error);
-      res.redirect('/')
-    }
+        // Look up the user from req.session
+        const currentUser = await User.findById(req.session.user._id);
+        // Render index.ejs, passing in all of the current user's 
+        // applications as data in the context object. 
+        res.render('applications/index.ejs', {
+          applications: currentUser.applications,
+        });
+      } catch (error) {
+        // If any errors, log them and redirect back home
+        console.log(error)
+        res.redirect('/')
+      }
   });
 
 module.exports = router
