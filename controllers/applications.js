85ceea3267b640428b6fad68b43788e0c7308031
Sleeper9235 <kylejyoumans@gmail.com>
2024-06-27 10:36:04 -0500

    create update page

--- controllers/applications.js
+++ controllers/applications.js
@@ -55,4 +55,18 @@ router.post('/', async (req, res) => {
         res.redirect('/')
     }
   })
+
+  router.get('/:applicationId/edit', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId)
+        res.render('applications/edit.ejs', {
+            application: application,
+        })
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+  })
+  
 module.exports = router
